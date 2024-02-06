c42d74c0e4a5d4d5190f010d5f4f7ae9c7a71f1c
Joshua Lanctot <joshua.lanctot10@gmail.com>
2024-02-06 13:44:23 -0600

    finish course 2

--- postgres/tutorial2.sql
+++ postgres/tutorial2.sql
@@ -322,4 +322,257 @@ WITH RECURSIVE series (list_num) AS (
 SELECT list_num FROM series;
 
 
--- Window functions
+-- Window functions (set of table rows over which function is applied)
+SELECT
+	name,
+	course,
+	ROW_NUMBER() OVER () AS rn
+FROM enrollees;
+
+SELECT
+	name,
+	course,
+	ROW_NUMBER() OVER (
+		PARTITION BY name -- number name by each course 
+		ORDER BY course
+	) AS rn
+FROM 
+	enrollees;
+	
+-- RANK() is same as ROW_NUMBER() except matching rows are ranked, DENSE_RANK() avoids gaps
+SELECT
+	name, RANK() OVER (
+		ORDER BY name -- for each matching name, give it the same number
+	) AS rank
+FROM enrollees;
+
+-- DENSE_RANK() if rows tie (both have 2), it assigned one column the differentiator
+SELECT
+	name,
+	RANK() OVER ( -- skips 2, 4, etc.
+		ORDER BY name) AS rank,
+	DENSE_RANK() OVER ( -- each name has same number
+		ORDER BY name) AS d_rank,
+	ROW_NUMBER() OVER ( -- row numbers each name/row
+		ORDER BY name) AS rn
+FROM enrollees;
+
+-- applying rank functions
+SELECT
+	mkt_carrier,
+	mkt_carrier_fl_num,
+	origin,
+	dest,
+	arr_delay_new
+FROM
+	performance
+WHERE fl_date = '2018-01-16'
+	AND mkt_carrier = 'AA'
+	AND ORIGIN = 'MCI';
+
+SELECT
+	mkt_carrier,
+	mkt_carrier_fl_num,
+	origin,
+	dest,
+	arr_delay_new,
+	ROW_NUMBER() OVER (ORDER BY CAST(mkt_carrier_fl_num AS int)) AS rn, -- look at flight data for the day, and sequentially number them based on flight number
+	DENSE_RANK() OVER (ORDER BY arr_delay_new DESC) AS delay_rank, -- rank numerically based on lowest to highest arrival time (DENSE_RANK fixes skipping issue with rank, use rank when you want same rank on duplicates)
+	DENSE_RANK() OVER (PARTITION BY dest ORDER BY arr_delay_new DESC) AS delay_rank -- in addition to delay time, airport is important consideration. Rank arrival delay for each destination airport
+FROM -- numbers all rows based on flight num
+	performance
+WHERE fl_date = '2018-01-16'
+	AND mkt_carrier = 'AA'
+	AND origin = 'MCI';
+	
+-- special values
+SELECT
+	FIRST_VALUE(name) OVER ( -- rank salary by department, highest value's name is given in col high_pay (LAST_VALUE for last val)
+			PARTITION BY dept
+			ORDER BY salary DESC
+	) AS high_pay
+FROM salaries;
+
+-- reuse window functions
+SELECT dept, 
+	LAST_VALUE(name) OVER w AS low_pay,
+	LAST_VALUE(salary) OVER w AS low_amt
+FROM salaries
+WINDOW w AS (
+	PARTITION BY dept
+	ORDER BY salary DESC -- rank salary from lowest to highest by department
+);
+
+-- lag and lead (lagging occurs before current row, leading occurs after)
+SELECT
+	sales as curr_month,
+	LAG(monthly_sales, 1) OVER ( -- show current record sales, then in prev_month column it is the previous month's sales
+		ORDER BY month
+	) AS prev_month
+FROM sales_record
+ORDER BY month;
+
+-- Aggregate functions
+-- average age per group
+SELECT
+	grade_lvl,
+	AVG(age) AS avg_age
+FROM students
+GROUP BY grade_lvl;
+
+-- average age per group but also retain rows
+SELECT
+	grade_lvl
+	AVG(age) OVER(
+		PARTITION BY grade_lvl
+	) AS avg_age
+FROM students;
+
+-- order of evaluation
+SELECT
+	a.first_name, a.last_name
+FROM (SELECT first_name,
+	 	last_name,
+	  ROW_NUMBER() OVER (
+	  	PARTITION BY dept
+		ORDER BY last_name
+	  ) AS rn
+	FROM students) a
+WHERE rn = 1; -- selects student with first last name in dept
+
+-- LAG function 
+SELECT
+	fl_date,
+	SUM(dep_delay_new) AS dep_delay
+FROM performance
+WHERE origin = 'DTW'
+GROUP BY fl_date;
+
+WITH daily_delays AS (
+	SELECT
+		fl_date,
+		SUM(dep_delay_new) AS dep_delay
+	FROM performance
+	WHERE origin = 'DTW'
+	GROUP BY fl_date
+)
+SELECT -- use lag function to compare a day to the previous day
+	fl_date,
+	dep_delay,
+	LAG(dep_delay, 1) OVER (ORDER BY fl_date) AS prior_day_delay,
+	dep_delay - LAG(dep_delay, 1) OVER (ORDER BY fl_date) AS change_delay
+FROM daily_delays;
+
+-- wrap up
+SELECT fl_date,
+	p.mkt_carrier,
+	c.carrier_desc,
+	p.mkt_carrier_fl_num,
+	p.origin,
+	p.origin_city_name,
+	p.dest,
+	p.dest_city_name,
+	p.dep_delay_new,
+	p.arr_delay_new
+FROM performance p
+JOIN codes_carrier c
+	ON p.mkt_carrier = c.carrier_code
+WHERE dest = 'ICT';
+
+-- Join
+SELECT 
+	CAST(AVG(performance.arr_delay_new) AS int) AS avg_delay_per_carrier, 
+	codes_carrier.carrier_desc
+FROM performance
+JOIN codes_carrier 
+	ON codes_carrier.carrier_code = performance.mkt_carrier
+GROUP BY codes_carrier.carrier_desc
+ORDER BY avg_delay_per_carrier;
+
+-- Union, subquery
+SELECT AVG(p.arr_delay_new) AS avg_arr_delay, dest_city_name
+FROM
+	(SELECT fl_date,
+		mkt_carrier,
+		mkt_carrier_fl_num,
+		origin,
+		origin_city_name,
+		dest,
+		dest_city_name,
+		dep_delay_new,
+		arr_delay_new
+	FROM performance 
+	UNION
+	SELECT fl_date,
+		mkt_carrier,
+		mkt_carrier_fl_num,
+		origin,
+		origin_city_name,
+		dest,
+		dest_city_name,
+		dep_delay_new,
+		arr_delay_new
+	FROM perform_feb 
+	WHERE dest = 'ICT') p
+WHERE p.arr_delay_new <> 0
+GROUP BY dest_city_name
+ORDER BY avg_arr_delay;
+
+-- CTE
+WITH flight_info AS (
+	SELECT * FROM performance
+	UNION
+	SELECT * FROM perform_feb
+)
+SELECT 
+	AVG(arr_delay_new) AS avg_arr_delay
+FROM flight_info;
+
+-- correlated subquery
+WITH flight_info AS (
+	SELECT *, 'Jan' AS fl_month FROM performance a WHERE a.dest = 'ICT'
+	UNION
+	SELECT *, 'Feb as fl_month' FROM perform_feb b WHERE b.dest = 'ICT'
+)
+SELECT 
+	p.fl_month,
+	p.fl_date,
+	p.mkt_carrier,
+	p.arr_delay_new
+FROM flight_info p
+WHERE p.arr_delay_new > (
+	SELECT AVG(arr_delay_new)
+	FROM flight_info p
+	WHERE p.mkt_carrier = mkt_carrier
+);
+
+
+WITH flight_info AS (
+	SELECT *, 'Jan' AS fl_month FROM performance a WHERE a.dest = 'ICT'
+	UNION
+	SELECT *, 'Feb as fl_month' FROM perform_feb b WHERE b.dest = 'ICT'
+)
+SELECT
+	pp.fl_month,
+	pp.fl_date,
+	pp.mkt_carrier,
+	pp.arr_delay_new,
+	REPLACE( (pp.origin_city_name || ' to ' || pp.dest_city_name), ',', '') AS route
+FROM
+	(SELECT 
+		p.fl_month,
+		p.fl_date,
+		p.mkt_carrier,
+		p.arr_delay_new,
+	 	p.origin_city_name,
+	 	p.dest_city_name,
+		DENSE_RANK() OVER (PARTITION BY fl_month ORDER BY arr_delay_new DESC) AS ranking
+	FROM flight_info p
+	WHERE p.arr_delay_new > (
+		SELECT AVG(arr_delay_new)
+		FROM flight_info p
+		WHERE p.mkt_carrier = mkt_carrier
+	)
+) pp
+WHERE pp.ranking < 4; -- window function to get top 3 longest arrival delays per month
+
